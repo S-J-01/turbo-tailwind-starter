0567bf73969363e892344c5bdfc5a4567acadc50
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-03-30 22:55:29 +0530

    cva

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Card } from "@repo/ui/card";
+import { Button } from "@repo/ui/button";
 
 
 
 export default function Page() {
   return (
-    <div className="border-red-300 border-4">
-      Hello world! This is turborepo with tailwind
-    </div>
+    <>
+    <Button intent={"primary"} size={"small"}>Click ME</Button>
+    <br />
+    <Button intent={"secondary"} size={"medium"}>Click ME</Button>
+    </>
   )
 }
