ecdcb8d1cff7050e45ff348befb2805151efa980
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-04-03 23:39:00 +0530

    cn function

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,6 +8,8 @@ export default function Page() {
   return (
     <>
    <Button onClick={()=>alert('hello')} >hello</Button>
+   <Button variant={"primary"} size={"sm"} >PrimarySmall</Button>
+   <Button variant={"secondary"} size={"lg"} className="bg-blue-700">SecondaryLarge</Button>
     </>
   )
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,22 +1,23 @@
-import { cva } from "class-variance-authority";
+import { cva, VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes, ReactNode } from "react"
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
+import cn from "../util/cn"
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants>{
 children:ReactNode;
 
 }
 
-export const Button = ({children, ...props}:ButtonProps)=>{
+export const Button = ({children,className,variant,size, ...props}:ButtonProps)=>{
 return(
-    <button className={twMerge(clsx(buttonVariants({size:"lg",type:"primary"})))}{...props}>{children}</button>
+    <button className={cn(buttonVariants({variant,size,className}))}{...props}>{children}</button>
 )
 }
 
 const buttonVariants = cva("ui-rounded-full",{
 variants:{
- type:{
+ variant:{
    "primary":"ui-border-2 ui-border-black ui-bg-slate-600 ui-text-white",
    "secondary":"ui-border-2 ui-border-black ui-bg-white ui-text-green"
  }  ,
